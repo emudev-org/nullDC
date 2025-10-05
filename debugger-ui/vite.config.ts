f1ad442d17318be517a0a9be881dbe8f59d51f66
Stefanos Kornilios Mitsis Poiitidis <skmp@emudev.org>
2025-10-05 23:33:03 +0300

    debugger: Don't fail build if git is missing / not building from git

--- debugger-ui/vite.config.ts
+++ debugger-ui/vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 import { execSync } from 'node:child_process';
 
 function resolveVersion(): string {
-  return execSync('git describe', { stdio: 'pipe' }).toString().trim() || "unknown";
+  try {
+    return execSync('git describe', { stdio: 'pipe' }).toString().trim() || "unknown";
+  } catch {
+    return "unknown";
+  }
 }
 
 const appVersion = resolveVersion();
