496d11246d74ba1792ac7db35607c73604381574
Stefanos Kornilios Mitsis Poiitidis <skmp@emudev.org>
2025-10-05 23:09:00 +0300

    debugger: remove non needed version scripts

--- debugger-ui/scripts/postbuild-version.js
+++
@@ -1,8 +0,0 @@
-import { readFileSync, writeFileSync } from "node:fs";
-import path from "node:path";
-
-const PACKAGE_JSON = path.resolve("package.json");
-const pkgRaw = readFileSync(PACKAGE_JSON, "utf8");
-const pkg = JSON.parse(pkgRaw);
-pkg.version = "__APP_VERSION__";
-writeFileSync(PACKAGE_JSON, `${JSON.stringify(pkg, null, 2)}\n`);
